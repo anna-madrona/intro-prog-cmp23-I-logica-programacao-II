ff902b6176b05c780403fef0065caf63a7d0aced
Anna Madrona <annajulia.batista@hotmail.com>
2023-06-06 21:09:07 -0300

    resolucao das atividades de logica de prof=gramacao I

---
+++ src/exercicios/condicionais/atividade-01/resposta.js
@@ -0,0 +1,9 @@
+let numero = 20
+
+if (numero > 20) {
+    console.log(`O número ${numero} é maior que 20.`)
+} else if (numero < 20) {
+    console.log(`O número ${numero} é menor que 20.`)
+} else {
+    console.log(`O número digitado é igual a ${numero}.`)
+}
